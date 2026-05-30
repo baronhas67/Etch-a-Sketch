b73fdba7835426625085cce20e67ec50b509e14b
Bhagath <baronhas67@gmail.com>
2026-05-30 15:00:29 +0530

    Add new random rgb colors feature

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 const boxHolder = document.querySelector("#box-holder");
 const button = document.querySelector("#newbtn");
 
+let color = "";
 let box = document.createElement("div");
 box.classList.add("boxes");
+
+function randomColor() {
+    let ranNum = Math.floor(Math.random() * 3) + 1;
+    console.log(ranNum);
+
+    if (ranNum == 3) {
+        color = "red";
+    } else if (ranNum == 2) {
+        color = "green";
+    } else {
+        color = "blue";
+    }
+}
 
 function setNewSize() {
     let newSize = +prompt("Enter new size: ", "");
@@ -36,5 +50,6 @@ function makeBox(max) {
 
 
 boxHolder.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "blue";
+    randomColor();
+    e.target.style.backgroundColor = color;
 })
